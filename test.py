90f074b0e5251217f1f452fca034c6a10d885865
lacruz1 <eduardlacruz799@gmail.com>
2026-05-20 15:45:01 -0400

    primeras prubeas de login fallido y exitoso

--- test.py
+++ test.py
@@ -8,5 +8,15 @@ class TestSistemaMedico(unittest.TestCase):
         self.auth_prueba = Autenticacion("admin", "1234")
         self.paciente_prueba = Paciente("juan", 25, "gripe", 70.8)
 
+    def test_login_exitoso(self):
+        #si los datos son validos devuelve true
+        self.assertTrue(self.auth_prueba.Validar_acceso("admin", "1234"))
+
+    def test_login_fallido(self):
+        #verifica que si la clave es la correcta
+        #si no lanza error
+        with self.assertRaises(ErrorDeLogin):
+            self.auth_prueba.Validar_acceso("admin", "clave_erronea")
+
 if __name__ == "__main__":
     unittest.main()
